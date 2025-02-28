93888c3fc1aa091b99fc96ab348055deb1c3d9f1
develloperdesli <desr7690@gmail.com>
2025-02-28 09:54:02 +0100

    mainpage and etc

---
+++ README.md
@@ -0,0 +1,4 @@
+# Zara
+Welcome to Zara our chatbot. 
+Check de algemene voorwaarden en privacybeleid voor dat je de chatbot gebruikt. 
+Als je de chatbot gebruikt ga je akoord met de algemene voorwaarden en het privacybeleid.
---
+++ main.py
@@ -0,0 +1,13 @@
+context = "The following is a conversation with an AI assistant.\nHuman: Hello, who are you?\nAI: I am Zara. How can I help you today? "
+conversation = {
+    "Conversation": ["Who are you?", "Hi! I am Zara. How can I help you today?"]
+}
+current_user_message = ""
+
+
+def chatinput():
+    user_input = input("")
+    user_input = current_user_message
+
+def output():
+    print(conversation)
