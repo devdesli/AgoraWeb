8fc408f93cf679a3d1f8ea67550dab834900517c
klungel2011 <rensvenema2011@gmail.com>
2025-06-06 09:55:39 +0200

    commit

--- models.py
+++ models.py
@@ -1,13 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
-import sqlalchemy as sa
 
 
 db = SQLAlchemy()
 
-class Like(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    todo_id = db.Column(db.Integer, db.ForeignKey('todo.id'), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    is_admin = db.Column(db.Boolean, default=False)
+    challenges = db.relationship('Todo', backref='author', lazy=True, cascade='all, delete-orphan')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,13 +33,21 @@ class Todo(db.Model):
     end_product = db.Column(db.Text, nullable=False)
     category = db.Column(db.String(50), nullable=False)
     image = db.Column(db.String(100), nullable=True)
+    approved = db.Column(db.Boolean, default=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f'<Todo {self.id}>'
 
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    todo_id = db.Column(db.Integer, db.ForeignKey('todo.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(100), nullable=False)
+    todo_id = db.Column(db.Integer, db.ForeignKey('todo.id'), nullable=False)
 
     def __repr__(self):
         return f'<Image {self.filename}>'
