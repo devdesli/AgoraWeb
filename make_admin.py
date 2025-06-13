37d4ac34057add194cd0a187ee8fdcaf7b387215
develloperdesli <develloperdesli@gmail.com>
2025-06-13 10:28:26 +0200

    Create make_admin.py

---
+++ make_admin.py
@@ -0,0 +1,13 @@
+from app import app, db
+from models import User
+
+with app.app_context():
+    username = input("input the username you want to make MASTER!")
+    user = User.query.filter_by(username=username).first()
+    if user:
+        user.is_admin = True
+        user.is_master = False
+        db.session.commit()
+        print(f'User {user.username} is now an ADMIN!')
+    else:
+        print({username} + 'not found')
