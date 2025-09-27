fed14697109599da5a2bde568c5cf4e0bce2abe7
devdesli <develloperdesli@gmail.com>
2025-09-27 23:25:54 +0200

    No SSL

--- app.py
+++ app.py
@@ -1083,4 +1083,4 @@ csrf.exempt(like_status)
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=5000, debug=False, ssl_context=("cert.pem", "key.pem"))
+    app.run(host="0.0.0.0", port=5000, debug=False)
