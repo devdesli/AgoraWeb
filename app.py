2f2d1cf83a73bb55841ab94f1817e6ced974486f
develloperdesli <develloperdesli@gmail.com>
2025-11-20 12:14:01 +0100

    Form for api update

--- app.py
+++ app.py
@@ -243,12 +243,13 @@ def my_challenges():
 @app.route('/api/admin/see-challenge/<int:user_id>')
 @login_required
 def api_admin_see_challenge(user_id):
+    form = DeleteForm()
     if not current_user.is_admin and not current_user.is_master:
         return "Unauthorized", 403
 
     user = User.query.get_or_404(user_id)
     challenges = Todo.query.filter_by(author_id=user.id).order_by(Todo.date_created.desc()).all()
-    return render_template('my_challenges.html', challenges=challenges)
+    return render_template('my_challenges.html', challenges=challenges, form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
