426b7b2f971ebd345387ea104521a5889c65150a
develloperdesli <develloperdesli@gmail.com>
2026-02-02 12:34:24 +0100

    Api Changes

--- api/rest_api.py
+++ api/rest_api.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, current_app, redirect, url_for
+from flask_login import current_user
 from flask_restful import Api, Resource
-from models import db, Todo
-
+from models import User, User, db, Todo
+import requests as request
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 api = Api(api_bp)
 
@@ -29,6 +30,24 @@ class ContributorApprove(Resource):
         except Exception:
             return {'message': 'Contributor approved', 'todo_id': todo.id}
 
+class Admin_Stats(Resource):
+    def get(self):
+        if current_user.is_master or current_user.is_admin:
+            link = "https://api.codetabs.com/v1/loc?github=DevDesli/AgoraWeb"
+            """Return some admin statistics."""
+            total_users = User.query.count()
+            total_todos = Todo.query.count()
+            total_likes = db.session.query(db.func.sum(Todo.likes)).scalar() or 0
+            pending_contributors = Todo.query.filter_by(contributor_approved=False).count()
+            code_size = request.get(link).text
+            return {
+                'total_users': total_users,
+                'total_todos': total_todos,
+                'likes': total_likes,
+                'pending_contributors': pending_contributors,
+                'code_size': code_size
+            }
 
+api.add_resource(Admin_Stats, '/admin/stats', endpoint='adminstats')
 api.add_resource(Hello, '/hello')
 api.add_resource(ContributorApprove, '/contributor/approve/<string:token>', endpoint='contributorapprove')
